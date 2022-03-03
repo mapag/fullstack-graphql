1399989ff1e2a831ab49ce67803fdec6855ff47f
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2022-03-03 17:25:48 -0300

    add schema

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,31 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+	type User {
+		id: ID!
+		username: String!
+	}
 
+	type Pet {
+		id: ID!
+		name: String!
+		type: String!
+		createdAt: String!
+	}
+
+	input PetInput {
+		name: String
+		type: String
+	}
+
+	type Query {
+		user: User
+		pets(input: PetInput): [Pet]!
+	}
+
+	# type Mutation {
+	# 	addPet(name: String!, type: String!): Pet
+	# }
 `;
 
 module.exports = typeDefs
