64c42becd81c96d2def3c148939bc32353d21dc7
Mateo Pagniez <62345597+mapag@users.noreply.github.com>
2022-03-04 18:29:38 -0300

    Add constants

--- api/src/server.js
+++ api/src/server.js
@@ -2,11 +2,12 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const {models, db} = require('./db')
+const constants = require('./constants')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: { models, db }
+  context: { models, db, constants }
 })
 
 server.listen().then(({ url }) => {
